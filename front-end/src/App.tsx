0213aff017b6c668de4e1087125c6a0bb650782f
Trần Triều Thuận <thuantrantrieu@tcom.vn>
2022-11-16 17:55:23 +0700

    fix bug

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,77 +3,84 @@ import io from "socket.io-client";
 import "./App.css";
 
 const socket = io("http://localhost:81", {
-	transports: ["websocket", "polling", "flashsocket"],
+  transports: ["websocket", "polling", "flashsocket"],
 });
 
 function App() {
-	const [text, setText] = useState("");
-	const [person, setPerson] = useState("");
-	const [check, setCheck] = useState(false);
-	const [messages, setMessages] = useState([]);
-	const send = async () => {
-		const date = new Date();
-		const h = date.getHours();
-		const m = date.getMinutes();
-		const time = h + ":" + m;
-		if (!person) alert("Please enter your name");
-		else {
-			if (text.trim().length > 0) {
-				const data = {
-					sender: person,
-					message: text,
-					time: time,
-				};
-				await socket.emit("messageToServer", data);
-			}
-		}
-	};
+  const [text, setText] = useState("");
+  const [person, setPerson] = useState("");
+  const [check, setCheck] = useState(false);
+  const [messages, setMessages] = useState<any>([]);
+  const send = async () => {
+    const date = new Date();
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const time = h + ":" + m;
+    if (!person) alert("Please enter your name");
+    else {
+      if (text.trim().length > 0) {
+        const data = {
+          sender: person,
+          message: text,
+          time: time,
+        };
 
-	useEffect(() => {
-		socket.on("messagesToClient", (data) => {
-			// setMessages((list) => [...list, data]);
-			// setMessages(data);
-			console.log(data);
-		});
-	}, [socket]);
+        await socket.emit("messageToServer", data);
+        setMessages((list: any) => [...list, data]);
+      }
+    }
+  };
 
-	return (
-		<div>
-			<div id="messages">
-				<>{console.log(messages)}</>
-			</div>
-			<div>
-				<div>
-					Name: {person}
-					{!check && (
-						<div>
-							Enter name:
-							<input
-								type="text"
-								value={person}
-								onChange={(e) => {
-									setPerson(e.target.value);
-								}}
-								onBlur={() => setCheck(true)}
-							/>
-						</div>
-					)}
-				</div>
-			</div>
-			<div>
-				<textarea
-					id="text"
-					value={text}
-					onChange={(e) => {
-						setText(e.target.value);
-					}}
-				></textarea>
-				<div>
-					<button onClick={send}>Submit</button>
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    socket.on("messageToClient", (data: any) => {
+      setMessages((list: any) => [...list, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      <div id="messages">
+        {messages &&
+          messages.map((data: any) => (
+            <>
+              <span>tên người gửi: {data.sender}</span> <br />
+              <span>message: {data.message}</span>
+              <br />
+            </>
+          ))}
+      </div>
+      <div>
+        <div>
+          Name: {person}
+          {!check && (
+            <div>
+              Enter name:
+              <input
+                type="text"
+                value={person}
+                onChange={(e) => {
+                  setPerson(e.target.value);
+                }}
+                onBlur={() => setCheck(true)}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <textarea
+          id="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></textarea>
+        <div>
+          <button onClick={send}>Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
