984d76f86f2329c8e34a015ee7040038d9029d67
haiduong <haiduong03102k@gmail.com>
2022-11-17 10:25:57 +0700

    ok

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import "./App.css";
+import { v4 } from "uuid";
+import { listenerCount } from "process";
 
 const socket = io("http://localhost:81", {
 	transports: ["websocket", "polling", "flashsocket"],
@@ -11,6 +13,14 @@ function App() {
 	const [person, setPerson] = useState("");
 	const [check, setCheck] = useState(false);
 	const [messages, setMessages] = useState<any>([]);
+
+	const result = messages.map((data: any) => (
+		<h4 key={data.id}>
+			<ul>tên người gửi: {data.sender}</ul>
+			<ul>message: {data.message}</ul>
+		</h4>
+	));
+
 	const send = async () => {
 		const date = new Date();
 		const h = date.getHours();
@@ -20,38 +30,25 @@ function App() {
 		else {
 			if (text.trim().length > 0) {
 				const data = {
+					id: v4(),
 					sender: person,
 					message: text,
 					time: time,
 				};
 
 				await socket.emit("messageToServer", data);
-				setMessages((list: any) => [...list, data]);
 			}
 		}
 	};
-
 	useEffect(() => {
-		socket.on("messageToClient", (data: any) => {
-			setMessages((list: any) => [...list, data]);
+		socket.on("messageToClient", (data) => {
+			setMessages(messages.concat([data]));
 		});
-	}, [socket]);
+	}, [socket, messages]);
 
 	return (
 		<div>
-			<div id="messages">
-				{messages &&
-					messages.map((data: any) => (
-						<>
-							<span>
-								tên người gửi: {data.sender}
-							</span>{" "}
-							<br />
-							<span>message: {data.message}</span>
-							<br />
-						</>
-					))}
-			</div>
+			<div id="messages">{result}</div>
 			<div>
 				<div>
 					Name: {person}
